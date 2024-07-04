f290e29907890a081474406ad73d5da2914979ad
Török Zoltán Ákos <yt9mho@inf.elte.hu>
2024-07-04 15:09:53 +0200

    Added progtételek

--- 02_programozasi_tetelek.py
+++ 02_programozasi_tetelek.py
@@ -6,3 +6,36 @@ my_numbers = [492, 188, 729, 423, 281, 564, 718, 409, 47, 730, 407, 246, 117, 19
               14, 564, 982, 723, 885, 449, 70, 368, 701, 104, 539, 778, 117, 402, 269, 939, 465, 320, 318, 703, 809,
               254, 749, 71, 880, 523, 223, 846, 218, 868, 728, 939, 176, 471, 240, 143]
 
+
+# MAXIMUM KIVÁLASZTÁS
+def my_max(my_numbers):
+
+    max_number = my_numbers[0]
+    for number in my_numbers:
+        if number > max_number:
+            max_number = number
+    return max_number
+
+# MINIMUM KIVÁLASZTÁS
+def my_min(my_numbers):
+
+    min_number = my_numbers[0]
+    for number in my_numbers:
+        if number < min_number:
+            min_number = number
+    return min_number
+
+
+# SZÁMLÁLÁS
+def my_even(my_numbers):
+
+    even_numbers = 0
+    for number in my_numbers:
+        if number % 2 == 0:
+            even_numbers = even_numbers+1
+    return even_numbers
+
+
+print(my_max(my_numbers))
+print(my_min(my_numbers))
+print(my_even(my_numbers))
