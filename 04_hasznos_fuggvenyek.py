aa61a1b1678878f7e187901eb4bdbc631c64c313
Török Zoltán Ákos <yt9mho@inf.elte.hu>
2024-07-09 15:41:40 +0200

    Reindented 04

--- 04_hasznos_fuggvenyek.py
+++ 04_hasznos_fuggvenyek.py
@@ -1,9 +1,9 @@
 # OLVASÁS
 f = open("./input/score.csv", "r")
 for x in f:
-  print(x.rstrip())  # rstrip kiszedi a /n-t!
-  currentline = x.split(",")
-  print(currentline[0])
-  print(currentline[1].rstrip())
+    print(x.rstrip())  # rstrip kiszedi a /n-t!
+    currentline = x.split(",")
+    print(currentline[0])
+    print(currentline[1].rstrip())
 f.close()
 
