4d53d9f8a3c37de6898787d2f8cfb377d33dd2cd
Török Zoltán Ákos <yt9mho@inf.elte.hu>
2024-07-11 14:48:24 +0200

    Fixed HW02

--- homework/02_get_longest_comment.py
+++ homework/02_get_longest_comment.py
@@ -1,4 +1,4 @@
 # A feladat: az alábbi REST API-n menjünk végig az összes poston és kommenten, és keressük meg a
 # legrövidebb és a leghosszabb bejegyzéseket! Ezeket a bejegyzéseket írjuk ki egy tetszőlegesen elnvezett file-ba az
-# ../output mappa alá!
+# ../output mappa (vagy más, egyénileg választott mappa) alá!
 # 'https://jsonplaceholder.typicode.com/'
