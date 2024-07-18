9c4b84387a42b1fadf829756e0be5499717918d5
Török Zoltán Ákos <yt9mho@inf.elte.hu>
2024-07-18 13:52:46 +0200

    Added weatherman task

---
+++ 06_weatherman/weatherman.py
@@ -0,0 +1,10 @@
+# Készítsünk egy python scriptet, ami service-ként folyamatosan futhat, és amennyiben azt érzékeli,
+# hogy a következő 2 órában esni fog az eső Budapesten, akkor automatikusan küldjön egy Windows notification üzenetet!
+# A feladathoz használjuk a win10toast pip module-t (a Windows notification-höz) és az openweathermap.org REST API-t az
+# időjárás adatokhoz! (Ide regisztálni kell, hogy legyen API kulcs a hívásokhoz! Napi 2000 hívás ingyenes, ez legyen
+# része a tervezésnek!)
+# Készítsünk a különböző részekhez tetszés szerinti python modulokat, és használjuk a funkcionális
+# programozási paradigmát!
+#
+# https://github.com/jithurjacob/Windows-10-Toast-Notifications
+# https://openweathermap.org/api/one-call-3
